4b9352bf0f1223978c8bea6c473fb8597a59a3a8
Ardaya Pratama <ardayaimamp@gmail.com>
2023-02-14 16:11:44 +0700

    Tugas soal 2 javascript by ardaya

---
+++ Soal2.js
@@ -0,0 +1,20 @@
+function himbuhan(a){
+    if (a > 1000) {
+      return "input tidak valid";
+    } else if (a == 1000 ){
+      return "RIBUAN";
+    } else if (a >= 100 && a < 1000){
+      return "RATUSAN";
+    } else if (a > 10 && a <= 99) {
+      return "PULUHAN";
+    } else if (a > 0 && a <= 9 ){
+      return "SATUAN";
+    }else {
+      return "Input tidak valid";
+    }
+  }
+  
+ 
+console.log(himbuhan(5))
+console.log(himbuhan(1000))
+console.log(himbuhan(-1000))
