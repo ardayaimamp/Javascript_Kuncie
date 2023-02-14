c9dca2c0a25fb49723e588a2575bd4ee3116b7e6
Ardaya Pratama <ardayaimamp@gmail.com>
2023-02-14 16:08:58 +0700

    Soal 1 materi javascript

---
+++ Soal1.js
@@ -0,0 +1,21 @@
+
+
+function countKata(str) {
+		
+	if(str) {
+		var obj = [];
+		for(let i = 0; i < str.length; i++) {
+            if(obj[str[i]]){ 
+                obj[str[i]] += 1; 
+			}else {
+				obj[str[i]] = 1;
+			}
+		}
+		console.log(obj);
+	}
+			
+}
+
+countKata("hello world")
+countKata("ardaya imam pratama")
+countKata("kuncie")
