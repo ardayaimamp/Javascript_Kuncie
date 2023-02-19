3bbafbef25f308a5b568f18201ee699bf83c74bd
Ardaya Pratama <ardayaimamp@gmail.com>
2023-02-19 12:56:00 +0700

    Uppdate variable a

--- Soal3.js
+++ Soal3.js
@@ -1,16 +1,15 @@
 function segitiga2(panjang) {
-    let a = "Kunciekunciekuncie"
+    let a = "kuncie"
     let hasil = '';
     for (let i = 0 ; i < panjang ; i++) {
-        for (let j = 0; j < i+1 ; j++) {
-            hasil += a[j] ;
-        }
-        hasil += '\n';
+        for (let j = 0; j < i+1; j++) {
+            hasil += a[j % a.length] ;
+        } 
+        hasil += "\n";
     }
     return hasil;
 }
 console.log(segitiga2(6));
-console.log(segitiga2(3));
-console.log(segitiga2(8));
-console.log(segitiga2(12));
+
+
  
